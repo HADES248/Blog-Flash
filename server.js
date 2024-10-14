c17deb3e26b5c87de387d2f25ea749793475a10b
HADES248 <shivasingh24803@gmail.com>
2024-10-14 23:12:43 +0530

    Using lodash methods.

--- server.js
+++ server.js
@@ -4,20 +4,26 @@
 
 // local packages (use git init) then download packages you need.
 // Using loadash (with git init too!)
+// During sharing this project you don't upload node_modules(they're too many) you simply upload this package.json which shows dependencies(then we can simply use npm install to install all 3rd party packages).
 
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash'); //using lodash
 
 const server = http.createServer((req, res) => {
 
+  //lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('Hello To you All!');
+  })
+  greet();
+  greet();
 
   res.setHeader('Content-Type', 'text/html');
-
-
-
   let path = './views/';
-
-
   switch (req.url) {
     case '/':
       path += 'index.html';
