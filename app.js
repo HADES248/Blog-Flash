4d0cdec125d1f27fc4bd0325c11953e462239f0e
HADES248 <shivasingh24803@gmail.com>
2024-10-23 22:44:39 +0530

    Installing & Using View Engine to render html pages

--- app.js
+++ app.js
@@ -7,23 +7,41 @@ const app = express();
 //listening for requests
 app.listen(3000);
 
+
+// View Engines - they help us inject dynamic data (like blogs - every one has different blogs) in our html page.
+// Express apps can use view engines very easily.
+// We will use EJS for this project.
+// To set up our view engine we use .set()
+app.set('view engine', 'ejs');
+// we can also store our views in different folder but we would need to specify the location of that folder.
+// app.set('view', 'myViews');
+
+
+
 //HOME PAGE
 // To respond we use app.(type of request get,post)
 // Takes 2 arguements, path and an call back function with req,res
 app.get('/', (req, res) => {
+  /*
+    Automatically sets the content-type so no setheader required & the statusCode.
+    res.send('<h1>Welcome to the Football Frontier International</h1>');
+    res.sendFile('./views/index.html', { root: __dirname });
+  */
 
-  // Automatically sets the content-type so no setheader required & the statusCode.
-  //res.send('<h1>Welcome to the Football Frontier International</h1>');
+  // To send a view as a response back to the browser we can use res.render().
+  res.render('index');
 
-  res.sendFile('./views/index.html', { root: __dirname });
 
 })
 
 //ABOUT PAGE
 app.get('/about', (req, res) => {
-  // To send an html file in res we can use .sendFile()
-  // One Problem, sendFile looks for an absolute path as a parameter from the root file computer, to define a relative path we have to specify where it is relative from using a second params
-  res.sendFile('./views/about.html', { root: __dirname });
+  /*
+    To send an html file in res we can use .sendFile()
+    One Problem, sendFile looks for an absolute path as a parameter from the root file computer, to define a relative path we have to specify where it is relative from using a second params
+    res.sendFile('./views/about.html', { root: __dirname });
+  */
+  res.render('about');
 })
 
 
@@ -32,11 +50,22 @@ app.get('./about-us', (req, res) => {
   res.redirect('/about');
 })
 
+
+// Creating a Blog
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
+})
+
+
+
 // 404 Page
 // In cases of 404 page we use .use() in express, this function fires for every request coming but only if the request reaches this point in the code (meaning if request finds the correct match it will send back the res and the req will end otherwise 404 page). Therefore, this should always be in the bottom of the code.
 app.use((req, res) => {
+  /*
   res.status(404).sendFile('./views/404.html', { root: __dirname });
   // Also need to set status here as express does not realize that anything is wrong here.
+  */
+  res.status(404).render('404');
 })
 
 
