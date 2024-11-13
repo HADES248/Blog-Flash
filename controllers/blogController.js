7585527f90eadb4659f4b893130844cf697eba54
HADES248 <shivasingh24803@gmail.com>
2024-11-13 17:27:51 +0530

    Using controller functions & importing code

---
+++ controllers/blogController.js
@@ -0,0 +1,53 @@
+const Blog = require('../models/blog');
+// Controllers is link between our models and our views, uses models to get data and then pass that data into a view.
+// Implementing controller functions such as
+// blogIndex, blogDetails, blogCreateGet, blogCreatePost, blogDelete
+
+const blogIndex = (req, res) => {
+
+  // Fetching all blogs and rendering them in index.ejs
+  // using .sort() method with the field createdAt(added automatically by mongoose) & -1 is newest to oldest sorting order.
+  Blog.find().sort({ createdAt: -1 }).then((result) => {
+    res.render('index', { title: 'All Blogs', blogs: result })
+  }).catch(err => console.log(err));
+
+}
+
+const blogDetails = (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id).then((result) => {
+    res.render('details', { title: 'Details', blog: result })
+  }).catch(err => console.log(err));
+
+}
+
+const blogCreateGet = (req, res) => {
+  res.render('create', { title: 'New Blog' });
+}
+
+const blogCreatePost = (req, res) => {
+  // We can directly send the req.body since req.body & blog schema is same.
+  const blog = new Blog(req.body);
+
+  blog.save().then((result) => {
+    // After saving the blog, redirected to home page to see the new blog.
+    res.redirect('/blogs');
+  }).catch(err => console.log(err));
+}
+
+const blogDelete = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id).then((result) => {
+    // When sending a Ajax request we cannot directly use a redirect as a res in node. So, we can send a res.json back to browser with a redirect property, then that property is sent to browser & then we will use Front-End to redirect to the next web page.
+    res.json({ redirect: '/blogs' })
+  }).catch(err => console.log(err));
+}
+
+module.exports = {
+  blogIndex,
+  blogDetails,
+  blogCreateGet,
+  blogCreatePost,
+  blogDelete
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,56 +1,27 @@
 const express = require('express');
 const Blog = require('../models/blog');
 const router = express.Router();
+const blogController = require('../controllers/blogController');
 
 // Moving all blog routes to a new folder to clean up code.
 // Using express method Router() for the routes, it creates an instance of express app then we export this instance to be used in the main app.
 
 // Blog routes - All the routes related to Blogs
 // Outputting the document data into the view.
-router.get('/', (req, res) => {
-  // Fetching all blogs and rendering them in index.ejs
-  // using .sort() method with the field createdAt(added automatically by mongoose) & -1 is newest to oldest sorting order.
-  Blog.find().sort({ createdAt: -1 }).then((result) => {
-    res.render('index', { title: 'All Blogs', blogs: result })
-  }).catch(err => console.log(err));
-})
+router.get('/', blogController.blogIndex);
 
 // Adding a blog
-router.post('/', (req, res) => {
-
-  // We can directly send the req.body since req.body & blog schema is same.
-  const blog = new Blog(req.body);
-
-  blog.save().then((result) => {
-    // After saving the blog, redirected to home page to see the new blog.
-    res.redirect('/blogs');
-  }).catch(err => console.log(err));
-});
+router.post('/', blogController.blogCreatePost);
 
 
 // Creating a Blog
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'New Blog' });
-})
+router.get('/create', blogController.blogCreateGet);
 
 // Detailing a blog
 // Route Parameters - Basically part of the route which is a variable, can be implemented using :variable name.
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-
-  console.log(id);
-  Blog.findById(id).then((result) => {
-    res.render('details', { title: 'Details', blog: result })
-  }).catch(err => console.log(err));
-})
+router.get('/:id', blogController.blogDetails);
 
 // Deleting a blog
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id).then((result) => {
-    // When sending a Ajax request we cannot directly use a redirect as a res in node. So, we can send a res.json back to browser with a redirect property, then that property is sent to browser & then we will use Front-End to redirect to the next web page.
-    res.json({ redirect: '/blogs' })
-  }).catch(err => console.log(err));
-})
+router.delete('/:id', blogController.blogDelete);
 
 module.exports = router;
